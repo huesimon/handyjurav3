5703d3cab5f3fdc93b35b7d06540d9c74e7e354b
AnthonMS <Anthon@Steiness.info>
2017-05-20 13:11:15 +0200

    Commit test Anthon

--- app/src/main/java/com/example/anthonsteiness/handyjuralayout/MainActivity.java
+++ app/src/main/java/com/example/anthonsteiness/handyjuralayout/MainActivity.java
@@ -25,6 +25,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     // Anthon's Commit test 18-05-2017 10:28
+    // Anthon's Commit test 20-05-2017 13:10
 
     String title = "HandyJura";
 
