9b68db8f7e16cb7b709067d3619efbf392377523
jibauch94 <jib090994@gmail.com>
2017-06-15 10:08:03 +0200

    ..

--- app/src/main/java/com/example/anthonsteiness/handyjuralayout/TaskActivity.java
+++ app/src/main/java/com/example/anthonsteiness/handyjuralayout/TaskActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 
 public class TaskActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
